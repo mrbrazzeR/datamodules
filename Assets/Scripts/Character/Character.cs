573046e533440e84e11b43de520dcf10ad5eec7a
hieu <nguyendinhhieu9a1@gmail.com>
2024-02-22 08:21:40 +0700

    finish

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,7 +1,64 @@
-﻿namespace Character
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Character
 {
-    public class Character
+    public class Character:MonoBehaviour
+    {
+        public int id;
+        public string nameCharacter;
+        [SerializeReference,SubclassSelector] public CharacterSkill skill;
+
+        private void Awake()
+        {
+            skill.Executed();
+        }
+    }
+
+    [Serializable]
+    public enum SkillType
+    {
+        Heal,
+        Atk, 
+        Def
+    }
+
+    [Serializable]
+    public abstract class CharacterSkill
+    {
+        public SkillType skillType;
+        public abstract void Executed();
+    }
+
+    [Serializable]
+    public class CharacterSkill1 : CharacterSkill
     {
         
+        public int def;
+        public override void Executed()
+        {
+            Debug.Log(def);
+        }
     }
+    [Serializable]
+    public class CharacterSkill2 : CharacterSkill
+    {
+        public int atk;
+        public float speed;
+        public override void Executed()
+        {
+            Debug.Log(atk);
+        }
+    }
+    [Serializable]
+    public class CharacterSkill3 : CharacterSkill
+    {
+        public int heal;
+        public override void Executed()
+        {
+            Debug.Log(heal);
+        }
+    }
+
 }
